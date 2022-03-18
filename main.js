f9c1d6ee991352d5a90f3a83eb6ec578dd651b1c
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-03-18 15:40:05 +0000

    First finished design

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-"use strict"
+"use strict";
 const changeAdviceBtn = document.querySelector("button.quote-toggle");
+let currentAdvice = document.querySelector("div.advice-quote span");
+let adviceID = document.querySelector(".advice-id");
+const adviceURL = "https://api.adviceslip.com/advice";
 
-const adviceURL = "";
-changeAdviceBtn.addEventListener("click", () => console.log("clicked"));
-let currentAdvice = document.querySelector("div.advice-quote p");
-// &ldquo; &rdquo;
+changeAdviceBtn.addEventListener("click", () => {
+  fetch(adviceURL)
+    .then(response => response.json())
+    .then(data => {
+      currentAdvice.textContent = `${data.slip.advice}`;
+      adviceID.textContent = `${data.slip.id}`;
+    })
+    .catch(err => console.error(err));
+});
