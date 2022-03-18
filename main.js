0a451bcf8f4e891dbdcbd58f7364e3bc2c8fdff6
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-03-18 08:58:05 +0000

    Initial commit (design for mobile)

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict"
+const changeAdviceBtn = document.querySelector("button.quote-toggle");
+
+const adviceURL = "";
+changeAdviceBtn.addEventListener("click", () => console.log("clicked"));
+let currentAdvice = document.querySelector("div.advice-quote p");
+// &ldquo; &rdquo;
